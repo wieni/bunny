bcb4fc539f787ce69a4daeaf64c3779ba6c2c272
Sander <sander@wieni.be>
2020-04-27 13:25:06 +0200

    Update bunny

--- resources/components/gallery/Gallery.jsx
+++ resources/components/gallery/Gallery.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Gallery = () => <div>I will be a gallery</div>;
+const Gallery = () => <div>I will be a nice, cool and nice fancy gallery</div>;
 
 export default Gallery;
